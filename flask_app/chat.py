6a8885b9b8418f5690aee62ea3fc9c917e97ee69
Murtaza <murtazajits@gmail.com>
2022-02-17 14:21:18 +0100

    removes subfolder

--- flask_app/chat.py
+++ flask_app/chat.py
@@ -10,9 +10,9 @@ print (os.getcwd())
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-with open('portfolio/intents.json', 'r') as json_data:
+with open('./intents.json', 'r') as json_data:
     intents = json.load(json_data)
-    FILE = "portfolio/data.pth"
+    FILE = "./data.pth"
     data = torch.load(FILE)
 
     input_size = data["input_size"]
